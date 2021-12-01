34617000b0789a402d516d23cdbd770a7c492ff0
SafaeQ <safaedani009@gmail.com>
2021-12-01 09:26:36 +0100

    calcul

---
+++ scriptCal.js
@@ -0,0 +1,78 @@
+let input = document.querySelector("#textview");
+// let number = document.querySelectorAll('.number');
+let result = 0;
+
+let operation = ["", null, ""];
+// let input = document.form.textview.value;
+let history = [];
+
+function operator(op) {
+    operation[1] = op;
+    console.log(operation);
+
+    if (operation[0] == "" && history.length > 0) {
+        operation[0] = history
+
+    }
+}
+
+function insert(number) {
+    if (operation[1] == null) {
+        operation[0] += "" + number;
+        input.value = operation[0];
+    } else {
+        operation[2] += "" + number;
+        input.value = operation[2];
+    }
+
+    console.log(operation);
+}
+
+function equal() {
+    if (!(operation[0] == "" || operation[2] == "" || operation[1] == null)) {
+        switch (operation[1]) {
+            case "*":
+                result = parseFloat(operation[0]) * parseFloat(operation[2]);
+                break;
+            case "/":
+                if (parseFloat(operation[0]) == 0 || parseFloat(operation[0]) == 0) {
+                    result = "Cannot devide by zero";
+                    break;
+                }
+                result = parseFloat(operation[0]) / parseFloat(operation[2]);
+                break;
+            case "+":
+                result = parseFloat(operation[0]) + parseFloat(operation[2]);
+                break;
+            case "-":
+                result = parseFloat(operation[0]) - parseFloat(operation[2]);
+                break;
+            case "%":
+                result = parseFloat(operation[0]) % parseFloat(operation[2]);
+                break;
+            default:
+                break;
+        }
+        input.value = result;
+        operation[0] = "";
+        operation[1] = null;
+        operation[2] = "";
+    } else {
+        input.value = "ERROR!!!";
+    }
+}
+
+function clearHitory() {
+    input.value = "";
+    operation = ["", null, ""];
+    console.log({
+        operation: operation,
+    });
+}
+// // console.log(clear());
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function (e) {
+    // console.log(e.target);
+    // document.getElementsById("main").style.color = "red";
+})
